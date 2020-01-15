aea21f7aa8c1a61263c4d76fa44e0533675d87f3
Kubilay Kocabal <kubilay.kocabal@hepsiburada.com>
2020-01-15 19:15:54 +0300

    :sunrise:

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    float x_Start = -6.3F, y_Start = 5.7F;
+    int columnLength, rowLength;
+    float x_Space = 0.5F, y_Space = 0.5F;
+    public GameObject prefab;
+    private Vector3[,] arrayMap;
+    private LineRenderer line;
+
+    public void Draw(int ColumnLength, int RowLenght)
+    {
+        columnLength = ColumnLength;
+        rowLength = RowLenght;
+
+        line = GetComponent<LineRenderer>();
+        arrayMap = new Vector3[rowLength, columnLength];
+
+        for (int row = 0; row < rowLength; row++)
+        {
+            for (int col = 0; col < columnLength; col++)
+            {
+                Vector3 dot = new Vector3(x_Start + (x_Space * row), y_Start + (-y_Space * col));
+                arrayMap[row, col] = dot;//col + 1;
+                Instantiate(prefab, dot, Quaternion.identity);
+            }
+        }
+        DrawLine(arrayMap[6, 4], arrayMap[3, 4], arrayMap[6, 8]);
+        DrawLine2(arrayMap[3, 4], arrayMap[8, 2]);
+    }
+
+    void DrawLine(Vector3 source, Vector3 middle, Vector3 destination)
+    {
+        line.positionCount = 5;
+        line.SetPosition(0, source);
+        line.SetPosition(1, middle);
+        line.SetPosition(2, destination);
+    }
+
+    void DrawLine2(Vector3 source, Vector3 destination)
+    {
+        line.SetPosition(3, source);
+        line.SetPosition(4, destination);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    public GameObject widthInput;
+    public GameObject heightInput;
+    private GridManager gridManager;
+    
+    void Awake()
+    {
+        gridManager = GameObject.FindObjectOfType<GridManager>();
+    }
+
+    public void Generate()
+    {
+        string _width = widthInput.GetComponent<InputField>().text;
+        string _height = heightInput.GetComponent<InputField>().text;
+
+        if(String.IsNullOrEmpty(_width) || String.IsNullOrEmpty(_height))
+        {
+            Debug.Log("Eksik girildi width height");
+        }
+
+        Size size = new Size();
+        if (int.TryParse(_width, out int width))
+        {
+            size.Width = width;
+        }
+
+        if (int.TryParse(_height, out int height))
+        {
+            size.Height = height;
+        }
+
+        gridManager.Draw(size.Width, size.Height);
+    }
+}
+
+public class Size
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
